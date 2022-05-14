776f9f3d41583efeb71556aafc9cabca15c48a9a
oyesa <mercyoyesa2018@gmail.com>
2022-05-14 12:54:41 +0300

    set up base template

--- app/views.py
+++ app/views.py
@@ -13,10 +13,29 @@ def index():
     title = 'Home - Welcome to THA TECH STREETS Blog'
     return render_template('index.html', title = title)
 
-@app.route('/blog/<blog_id>')
-def blog(blog_id):
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('blog.html',id = blog_id)
+# @app.route('/blog/<blog_id>')
+# def blog(blog_id):
+
+#     '''
+#     View movie page function that returns the blog page details page and its data
+#     '''
+#     return render_template('blog.html',id = blog_id)
+
+
+# @app.route('/about/<about_id>')
+# def about(about_id):
+
+#     '''
+#     View movie page function that returns the about page details page and its data
+#     '''
+#     return render_template('about.html',id = about_id)
+
+
+# @app.route('/add/<add_id>')
+# def add(add_id):
+
+#     '''
+#     View movie page function that returns the add blog details page and its data
+#     '''
+#     return render_template('add.html',id = add_id)
