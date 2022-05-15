91beea997a46a678f6719fa0eadb4b44969c7373
oyesa <mercyoyesa2018@gmail.com>
2022-05-15 19:10:15 +0300

    second migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 # from flask_uploads import UploadSet, configure_uploads, IMAGES
 
@@ -8,6 +9,7 @@ from flask_bootstrap import Bootstrap
 #initialize extensions
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
 
 
 
