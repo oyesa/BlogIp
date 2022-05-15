8e736aeeb2fd6d4ec22711c77e490cbbf198b3e6
oyesa <mercyoyesa2018@gmail.com>
2022-05-15 18:49:37 +0300

    define methods for user class

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 from datetime import datetime
 
 
 
 
-# Roles table
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,22 +17,54 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
-#user table
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), index=True)
-    name = db.Column(db.String(255))
-    # email = db.Column(db.String(255), unique=True, index=True)
-    # password_hash = db.Column(db.String(255))
-    # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    # bio = db.Column(db.String(255))
-    # profile_pic_path = db.Column(db.String())
-    # pass_secure = db.Column(db.String(255))
-    # created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    # posts = db.relationship('Post', backref='user', lazy='dynamic')
-    # comments = db.relationship('Comment', backref='user', lazy='dynamic')
+    username = db.Column(db.String(50), index=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, index=True)
+    password_hash = db.Column(db.String(50))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # search email in db
+    @classmethod
+    def search_email(cls, email):
+        user = cls.query.filter_by(email=email).first()
+        if user:
+            return user
+
+    # search username in db
+    @staticmethod
+    def search_username(username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return user
 
     def __repr__(self):
         return f'User {self.username}'
